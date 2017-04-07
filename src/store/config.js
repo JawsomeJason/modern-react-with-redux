449f64f56ab6affff7c8604f22078e8c4ef7fac8
Jason T. Featheringham <jasonf@nerdwallet.com>
2017-04-07 02:59:58 -0700

    lint fixes

--- src/store/config.js
+++ src/store/config.js
@@ -2,10 +2,15 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../store/reducers/index';
 
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState);
+  const middleware = [];
+
+  const createStoreWithMiddleware = compose(
+    applyMiddleware(...middleware),
+  )(createStore);
+
+  const store = createStoreWithMiddleware(rootReducer, initialState);
 
   if (module.hot) {
-    console.log('is hot');
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../store/reducers', () => {
       // eslint-disable-next-line global-require
